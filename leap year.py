1c084ec69bcc232e68a4d11b72003f10d5ffa13b
JessaTehCrow <jellekok.3.4@gmail.com>
2021-10-01 16:35:49 +0200

    Simplified, and upated user input

--- leap year.py
+++ leap year.py
@@ -1,17 +1,15 @@
 # Python program to check if year is a leap year or not
 
-year = 2000
+year = 300
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+# To get user input, un-comment code below
 
-if (year % 4) == 0:
-   if (year % 100) == 0:
-       if (year % 400) == 0:
-           print("{0} is a leap year".format(year))
-       else:
-           print("{0} is not a leap year".format(year))
-   else:
-       print("{0} is a leap year".format(year))
+# year = input("Year: ")
+# if not year.isnumeric():
+#     exit("Invalid number")
+# year = int(year)
+
+if not year%100 == 0 and year%4 == 0 or year % 400 == 0:
+    print(f"{year} Is a leap year")
 else:
-   print("{0} is not a leap year".format(year))
+    print(f"{year} Is not a leap year")
