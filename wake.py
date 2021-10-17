a77b61050031ce8f13a5eed2ffb78408f730dd29
Sattik-Tarafder <92642327+Sattik-Tarafder@users.noreply.github.com>
2021-10-17 21:51:08 +0530

    Added wake.py
    This program will stop your computer from going to sleep mode

---
+++ wake.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+import argparse
+import sys
+
+pyautogui.FAILSAFE = False
+
+count = 0
+
+def wake(args):
+    global count
+    while True:
+        time.sleep(args.time)
+        for i in range(0, 100):
+            pyautogui.moveTo(0, i * 5)
+        if args.v == 2:
+            count += 1
+            print(count)
+        else:
+            pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-time','-t', type=float, default=30.0, help="Time(seconds) to wait before moving the mouse to keep the computer wake. Default is 30 seconds")
+
+    parser.add_argument('-v', type=int, default=1, help="This defines verbosity level. This is for to see how many times the mouse has moved. Default is 1 and maximum is 2")
+
+    args = parser.parse_args()
+
+    sys.stdout.write(str(wake(args)))
