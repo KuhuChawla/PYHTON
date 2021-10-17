94862d195deec6f421e94a8e9bc274f27b29c7b7
Sattik-Tarafder <92642327+Sattik-Tarafder@users.noreply.github.com>
2021-10-17 22:35:08 +0530

    Update wake.py
    Added key press

--- wake.py
+++ wake.py
@@ -13,6 +13,8 @@ def wake(args):
         time.sleep(args.time)
         for i in range(0, 100):
             pyautogui.moveTo(0, i * 5)
+        for i in range(0, 3):
+            pyautogui.press('shift')
         if args.v == 2:
             count += 1
             print(count)
@@ -23,7 +25,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-time','-t', type=float, default=30.0, help="Time(seconds) to wait before moving the mouse to keep the computer wake. Default is 30 seconds")
 
-    parser.add_argument('-v', type=int, default=1, help="This defines verbosity level. This is for to see how many times the mouse has moved. Default is 1 and maximum is 2")
+    parser.add_argument('-v', type=int, default=1, help="This defines verbosity level. This is for to see how many times the wake function has run. Default is 1 and maximum is 2")
 
     args = parser.parse_args()
 
